358fa91c5dd5e24ee53cb52d5e55b0f319e13eb1
Tino <155625923+Meowgoshi@users.noreply.github.com>
2024-01-09 11:25:34 +0000

    03 / Wertzuweisung II + Datentypen

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -12,9 +12,31 @@
 
 /* Deklaration + Wertzuweisung I */
 
-let firstName; // Deklaration (Definition)
+//let firstName; // Deklaration (Definition)
 //console.log(firstName);
-firstName = "Max"; // Wertzuweisung (assignment)
+//firstName = "Max"; // Wertzuweisung (assignment)
 //console.log(firstName);
-let familyName = "Mütze"; // Deklaration + Wertzuweisung
-console.log("Hi, ich bin " + firstName + " " + familyName + "!");
+//let familyName = "Mütze"; // Deklaration + Wertzuweisung
+//console.log("Hi, ich bin " + firstName + " " + familyName + "!");
+
+/* 
+Wertzuweisung II + Datentypen
+JS ist eine untypisierte Sprache
+*/
+
+let test; // Deklaration
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("------------");
+test = "hello"; // string
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("------------");
+test = 2; // number
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("------------");
+test = true; // boolean
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+
