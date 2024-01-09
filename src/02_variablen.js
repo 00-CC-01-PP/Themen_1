afbb25ea383f6555c80fd59806c9f368bd4d53fc
Tino <155625923+Meowgoshi@users.noreply.github.com>
2024-01-09 10:51:12 +0000

    02 / Deklaration und Wertzuweisung I

---
+++ src/02_variablen.js
@@ -0,0 +1,20 @@
+
+// Zeilenkommentar
+
+/* 
+    Blockkommentar 
+*/
+
+/***** Variablen 01 *******/
+
+//console.log("Hi!"); // Ausgabe
+//console.log(Hi); // " " fehlt und sorgt für Fehlermeldung
+
+/* Deklaration + Wertzuweisung I */
+
+let firstName; // Deklaration (Definition)
+//console.log(firstName);
+firstName = "Max"; // Wertzuweisung (assignment)
+//console.log(firstName);
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log("Hi, ich bin " + firstName + " " + familyName + "!");
