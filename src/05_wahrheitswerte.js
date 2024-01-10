5ef8cc89bf62d7378185da5a8e5e548b2ad3ebca
Tino <155625923+Meowgoshi@users.noreply.github.com>
2024-01-10 11:34:13 +0000

    05 / wahrheitswerte_alter

---
+++ src/05_wahrheitswerte.js
@@ -0,0 +1,15 @@
+
+/*  Berechnung Alter    */
+
+    //  Deklaration
+    let ageJohn, ageMark;
+    const birthYearJohn = 2000;
+    const birthYearMark = 1990;
+
+    //  Berechung Alter
+    ageJohn = 2024 - birthYearJohn;
+    ageMark = 2024 - birthYearMark;
+
+    // Ausgabe
+    console.log("John: " + ageJohn);
+    console.log("Mark: " + ageMark);
