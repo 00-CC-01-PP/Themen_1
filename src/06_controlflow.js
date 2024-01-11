0686a3c61ea38098957497fc2d465e8ecd658512
Tino <155625923+Meowgoshi@users.noreply.github.com>
2024-01-11 10:05:46 +0000

    06 / if-else-if

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -31,10 +31,32 @@ isJohnEqual = (ageJohn == ageMark);
     console.log("John ist älter!");
 }*/
 
-if (ageJohn > ageMark)
+/*if (ageJohn > ageMark)
 {
     console.log("John ist älter!");
 } 
+else 
+{
+    console.log("John ist jünger!");
+}*/
+
+/*********  Ternäre (ternary) Schreibweise **********/
+//console.log((isJohnOlder) ? "John is older" : "John is younger");
+
+/************ IF - ELSE IF  ************/
+// mit alternativen Fällen (älter, jünger, gleich alt)
+
+if (isJohnOlder)
+// Ja-Zweig
+{
+    console.log("John ist älter!");
+} 
+// alternative Zweig ... 1
+else if (isJohnEqual)
+{
+    console.log("John ist gleich alt!");
+}
+// gemeinsame Alternative
 else 
 {
     console.log("John ist jünger!");
