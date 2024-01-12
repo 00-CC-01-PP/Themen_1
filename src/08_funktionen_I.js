647722d0c9b8511b63614f9bff0d5255381768e1
Tino <155625923+Meowgoshi@users.noreply.github.com>
2024-01-12 09:58:42 +0000

    08 / parametrisierung

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -1,15 +1,40 @@
-
-
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
 // Funkionsaufruf | Call
-test();
+//test();
 
 // Funktionsrumpf | callee
 // Funktionsdeklaration
-function test()
+//function test()
+//{
+//    console.log("Katzen sind toll!");
+//}
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+//Call
+//ausgabeNamen();
+
+// Funktion
+//function ausgabeNamen() 
 {
-    console.log("Katzen sind toll!");
+    // interne Variable | What happens in Vegas...
+//    let firstName = "Ripley";
+//    console.log("Hey " + firstName + "!");
 }
 
+// console.log(firstName); // Fehler scope
+
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+ausgabeNamen2("Yuffie");
+ausgabeNamen2("Legoshi");
+ausgabeNamen2("Ripley");
+
+function ausgabeNamen2(firstName) // Parameter
+{
+    console.log("Hey " + firstName + "!");
+}
