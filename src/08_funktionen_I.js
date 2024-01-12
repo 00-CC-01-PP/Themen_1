5aa69ec2756c8ce686d390bb50ae97a2f7e2317a
Tino <155625923+Meowgoshi@users.noreply.github.com>
2024-01-12 09:18:16 +0000

    08 / kapselung

---
+++ src/08_funktionen_I.js
@@ -0,0 +1,15 @@
+
+
+/***** Funktionen 01 *****/
+// 1. Kapselung von Codeblöcken
+
+// Funkionsaufruf | Call
+test();
+
+// Funktionsrumpf | callee
+// Funktionsdeklaration
+function test()
+{
+    console.log("Katzen sind toll!");
+}
+
