8ae08d830d542e435ef83e60136e5ff5a21d1b9d
Tino <155625923+Meowgoshi@users.noreply.github.com>
2024-01-17 10:54:44 +0000

    11 / drei Fälle

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -6,8 +6,47 @@
  * 
  */
 
+/********* Überlegungen - Transponierung **********/
+ 
+/*
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
 
+/* Zu "Fuß" - DRY !!! */
+/*
+ let a = 0; // Anfangswert
+ output("inhalt von a: " + a);
+ a = a + 1; // a = 0 + 1
+ output("inhalt von a: " + a);
+ a = a + 1; // a = 1 + 1
+ output("inhalt von a: " + a);
+ a = a + 1; // a = 2 + 1
+ output("inhalt von a: " + a);
+ */
 
+ /* Besser: mit FOR-Schleife */
+/*
+let a = 0; // Anfangswert
+for (let i = 0; i < 10; i++)
+{
+    a += 1; // a = a + 1
+    output("Inhalt von : " + a);
+}
+*/
+
+/* 1a. Einer Variablen kann Ihr eigener Wert zugewiesen werden
+Solange die Variable existiert, bleibt dieser erhalten
+hier: Verkettung eines Strings // Transponierung */
+/*
+let str = ""; // Anfangswert
+for (let i = 0; i < 10; i++)
+{
+    str += "Test" + " "; // a = a + 1
+    output(str);
+}
+*/
 /**
  *   PRAXIS
  * 
@@ -107,11 +146,66 @@ for (let i = 10; i > 0; i--)
 */
 
 // Inkrement (var. Schrittweite)
-
-for (let i = 10; i <= 100; i -= 5)
+/*
+for (let i = 10; i >= 100, i >= -100; i -= 5)
 {
     output(i);
 }
+*/
+/* 02b. For-Schleife für Array-Index (Iteration)*/
+/*
+let arr = ["Ich","bin","die","coole","Ellen","Ripley"];
+for (let i = 0; i < arr.length; i++)
+{
+    //output(i);
+    output(arr[i]);
+}
+*/
+
+/*** 01b. Funktionalität mit Array 2  */
+// Transponierung:  untereinander ---> nebeneinander
+// Helge Schneider: Anananandereihung ...
+/*
+output(getSentenceArr2(["Ich","bin","die","coole","Ellen","Ripley"]));
+function getSentenceArr2(arr) 
+{
+        const GAP = " ";
+        const PUNCT = ".";
+        let str;
+
+        for (let i = 0; i < arr.length; i++)
+        {
+            output(arr[i]);
+        }
+        return str;
+}
+*/
+output(getSentenceArr2(["Ich","bin","die","coole","Ellen","Ripley"]));
+function getSentenceArr2(arr) 
+{
+        const GAP = " ";
+        const PUNCT = ".";
+        let str = "";
+
+        for (let i = 0; i < arr.length; i++)
+        {
+            // 1. Fall
+            //str += arr[i] + GAP;
+            // 2. Fall
+            //str += arr[i] + PUNCT;
+            // 3. Fall
+            if (i != arr.length-1) 
+            {
+               str += arr[i] + GAP; 
+            } 
+            else 
+            {
+                str += arr[i] + PUNCT;
+            }
+        }
+        return str;
+}
+
 
 function output(outputStr) {
     console.log(outputStr);
